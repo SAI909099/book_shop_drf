e2224364fb13b8e7e8796b28ab4a17b601f36d0a
SAI909099 <sulaymonovabdulaziz1@gmail.com>
2024-10-10 19:13:06 +0500

    settings to'g'itlandi

--- root/settings.py
+++ root/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'mptt',
 ]
 AUTH_USER_MODEL = 'users.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
